f93ca5706f1d26b97d781a35fabb6ab08ee2724d
meiseayoung <meiseayoung@gmail.com>
2017-11-28 09:31:41 +0800

    v0.1

---
+++ BaseComponent.js
@@ -0,0 +1,158 @@
+var BaseComponent = Class.extend({
+    /**
+     * 初始化组件事件
+     */
+    init:function(opts){
+       var defaultConfig = {
+           handlers:{}
+       };
+       this.config = Object.assign(defaultConfig,opts);
+    },
+	/**
+	 * 功能说明： 更新组件 子组件自行实现
+	 */
+	update: $.noop,
+	/**
+	 * 功能说明： 消毁组件 子组件自行实现
+	 * 参数说明：
+	 * 无参数
+	 **/
+	destroy: $.noop,
+	/**
+	 * 功能说明 ：添加自定义事件
+	 * 参数说明 ：
+	 * @param type [String] 自定义事件类型(名称)
+	 * @param hander [Function] 自定义事件类型对应的事件执行函数
+	 **/
+	on: function (type, handler) {
+		var me = this;
+		if (typeof me.config.handlers[type] == "undefined") {
+			me.config.handlers[type] = [];
+		}
+		me.config.handlers[type].push(handler);
+	},
+	/**
+	 * 功能说明 ：触发自定义事件
+	 * 参数说明 ：
+	 * @param type [String] 自定义事件类型(名称)
+	 * @param data [Function] 自定义事件类型对应的事件执行函数的参数
+	 * @return undefined 无返回值
+	 **/
+	fire: function (type, data) {
+		var me = this;
+		if (me.config.handlers[type] instanceof Array) {
+			var handlers = me.config.handlers[type];
+			for (var i = 0; i < handlers.length; i++) {
+				handlers[i](data);
+			}
+		}
+	},
+	/**
+	 * 功能说明 ：移除事件
+	 * 参数说明 ：
+	 * @param type [String] 事件类型(名称)
+	 * @param handler [Function] 事件处理函数
+	 * @return undefined 无返回值
+	 **/
+	off: function (type, handler) {
+		var me = this;
+		var handlers = this.handlers[type];
+		if (me.handlers[type] instanceof Array) {
+			for (var i = 0; i < handlers.length; i++) {
+				if (handlers[i] === handler) {
+					break;
+				}
+			}
+		}
+		handlers.splice(i, 1);
+	},
+	/**
+	 * 功能说明 : 显示组件UI
+	 * 参数说明 ：
+	 * @param isAnimate [Boolean] 是否开启动画效果
+	 * @return undefined 无返回值
+	 **/
+	show: function (isAnimate) {
+		var me = this;
+		if (isAnimate === true) {
+			$(me.config.renderTo).slideDown(400)
+		} else {
+			$(me.config.renderTo).show();
+		}
+	},
+	/**
+	 * 功能说明 : 隐藏组件UI
+	 * 参数说明 ：
+	 * @param isAnimate [Boolean] 是否开户动画效果
+	 * @return undefined 无返回值
+	 **/
+	hide: function (isAnimate) {
+		var me = this;
+		if (isAnimate === true) {
+			$(me.config.renderTo).slideUp(400);
+		} else {
+			$(me.config.renderTo).hide();
+		}
+	},
+	/**
+	 * 功能说明 : 设置组件UI高度
+	 * 参数说明 ：
+	 * @param height [Number] 高度
+	 * @return undefined 无返回值
+	 **/
+	setHeight: function (height) {
+		var me = this;
+		$(me.config.renderTo).css({
+			height: height
+		});
+	},
+	/**
+	 * 功能说明 : 设置组件UI高度
+	 * 参数说明 ：
+	 * @param width [Number] 宽度
+	 * @return undefined 无返回值
+	 **/
+	setWidth: function (width) {
+		var me = this;
+		$(me.config.renderTo).css({
+			width: width
+		});
+	},
+	/**
+	 * 功能说明 : 设置组件UI高度
+	 * 参数说明 ：
+	 * 无参数
+	 * @return undefined 无返回值
+	 **/
+	getHeight: function () {
+		var me = this;
+		var height = $(me.config.renderTo).height();
+		return height;
+	},
+	/**
+	 * 功能说明 : 设置组件UI高度
+	 * 参数说明 ：
+	 * 无参数
+	 * @return undefined 无返回值
+	 **/
+	getWidth: function () {
+		var me = this;
+		var width = $(me.config.renderTo).width();
+		return width;
+	},
+	/**
+	 * 功能说明 : 容器发生尺寸变化时的回调
+	 * 参数说明 ：
+	 * 无参数
+	 * @return undefined 无返回值
+	 **/
+	_resize: $.noop,
+	/**
+	 * 功能说明 : 初始化组件名
+	 * 参数说明 ：
+	 * 无参数
+	 **/
+	_setWidgetClass: function () {
+		var me = this;
+	}
+});
---
+++ List/List.js
@@ -0,0 +1,101 @@
+var List = BaseComponent.extend({
+    /**
+     * 初始化组件
+     */
+    init: function (opts) {
+        var defaultConfig = {
+            renderTo: null,
+            handlers: {},
+            // list: [{
+            //     name: "北京人民医院",
+            //     value: 6344,
+            //     percent: 75,
+            //     unit: ""
+            // }],
+            list:[],
+            itemHeight: 24,
+            itemMargin: 5,
+            itemTextAlign: "left",
+            itemNameWidth: 80,
+            itemPercentWidth: 300,
+            itemValueWidth: 100,
+            itemBorderRadius:true,
+            itemPercentBackendHeight:8,
+            itemPercentFrontendHeight:10,
+            itemPercentBackendColor: "#204f7a",
+            itemPercentFrontEndColorStart: "#0096f7",
+            itemPercentFrontEndColorEnd: "#00dee1",
+            itemNameColor: "#c8c6d5",
+            itemValueColor: "#c8c6d5"
+        };
+        this.config = $.extend(defaultConfig, opts);
+        this._createDOM();
+        this._addBehavior();
+    },
+    /**
+     * 更新组件
+     */
+    update: function (list) {
+        if ($.type(list) !== "array") {
+            console.warn("更新组件出错,更新方法传入参数类型为数组,请检查");
+            return;
+        }
+        this.config.list = list;
+        this._createDOM();
+    },
+    /**
+     * 创建列表DOM元素
+     */
+    _createDOM: function () {
+        var me = this;
+        if ($(me.config.renderTo).length === 0) {
+            console.warn("组件渲染目标不存在,请检查传入的renderTo配置项");
+            return;
+        };
+        var listHTML = me.config.list.map(function (v, i) {
+            return `<li data-index=${i} style="height:${me.config.itemHeight}px;margin:${me.config.itemMargin}px 0">${me._createItem(v)}</li>`;
+        }).join("");
+        $(me.config.renderTo).html(`<ul class="custom-list">${listHTML}</ul>`);
+        (function (me) {
+            setTimeout(function () {
+                $(me.config.renderTo).find(".list-item-percent-frontend").each(function (index, elem) {
+                    var elemWidth = $(elem).attr("data-percent");
+                    $(elem).css({
+                        width: elemWidth + "%"
+                    });
+                })
+            }, 100)
+        })(me);
+
+
+    },
+    /**
+     * 创建列表项
+     */
+    _createItem: function (item) {
+        var me = this;
+        var itemHTML = `<div class="list-item" style="height:100%;">
+                            <div class="list-item-name" style="width:${me.config.itemNameWidth}px;text-align:${me.config.itemTextAlign};line-height:${me.config.itemHeight}px;color:${me.config.itemNameColor}"><span>${item.name}</span></div>
+                            <div class="list-item-percent" style="position:relative;width:${me.config.itemPercentWidth}px;">
+                                <div class="list-item-percent-backend" style="position:absolute;height:${me.config.itemPercentBackendHeight}px;top:${ (me.config.itemHeight -me.config.itemPercentBackendHeight)/2 }px;width:100%;background-color:${me.config.itemPercentBackendColor};"></div>
+                                <div class="list-item-percent-frontend" style="position:absolute;height:${me.config.itemPercentFrontendHeight}px;top:${ (me.config.itemHeight -me.config.itemPercentFrontendHeight)/2 }px;width:${0}%;background-image:linear-gradient(to right, ${me.config.itemPercentFrontEndColorStart}, ${me.config.itemPercentFrontEndColorEnd});border-radius:0 ${ me.config.itemBorderRadius ? me.config.itemHeight / 2 : 0}px ${me.config.itemBorderRadius ? me.config.itemHeight / 2 :0}px 0" data-percent=${item.percent || 0}></div>
+                            </div>
+                            <div class="list-item-value" style="width:${me.config.itemValueWidth}px;line-height:${me.config.itemHeight}px;color:${me.config.itemValueColor}"><span>${item.value}</span><span>${item.unit || ""}</span></div>
+                        </div>`;
+        return itemHTML;
+    },
+    /**
+     * 添加组件行为
+     */
+    _addBehavior: function () {
+        var me = this;
+        if ($(this.config.renderTo).length === 0) {
+            console.warn("组件渲染目标不存在,请检查传入的renderTo配置项");
+            return;
+        }
+        $(this.config.renderTo).on("click", "li", function (e) {
+            var index = $(this).attr("data-index");
+            me.fire("itemClick", me.config.list[index]);
+        });
+    }
+})
